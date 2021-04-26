e5718d9928b1e32530773cdd6c21274a01afad0d
Pol Roca <pol.roca@estudiants.urv.cat>
2021-04-26 23:05:32 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import os
+import time
+import numpy as np
+import pandas as pd
+
+from tree import DecisionTree, RandomTree
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import click
+from click import Choice
+
+
+def load_hepatitis():
+    path = os.path.join('data', 'hepatitis.csv')
+    df = pd.read_csv(path)
+    return df
+
+
+def load_cmc():
+    path = os.path.join('data', 'cmc.csv')
+    df = pd.read_csv(path)
+
+    df = df.astype(str)
+    num_attr = ['Wifes_age', 'Number_of_children_ever_born']
+    df[num_attr] = df[num_attr].astype(int)
+    return df
+
+
+def load_nursery():
+    path = os.path.join('data', 'nursery.csv')
+    df = pd.read_csv(path)
+    return df
+
+
+@click.command()
+@click.option('-d', '--dataset', type=Choice(['hepatitis', 'cmc', 'nursery']), required=True,
+              help='Dataset name.')
+@click.option('-s', '--seed', type=int, default=101, show_default=True, help='Random seed used when'
+              ' shuffling data before splitting it into train and test sets.')
+def main(dataset, seed):
+    # load the corresponding dataset into a dataframe
+    if dataset == 'hepatitis':
+        df = load_hepatitis()
+        target = 'class'
+
+    elif dataset == 'cmc':
+        df = load_cmc()
+        target = 'Contraceptive_method_used'
+
+    elif dataset == 'nursery':
+        df = load_nursery()
+        target = 'class'
+
+    df_train, df_test = train_test_split(df, test_size=0.2, shuffle=True, random_state=seed)
+    df_train = df_train.copy()
+    df_test = df_test.copy()
+
+    # preprocessing - fill missing values
+    num_attr = df.select_dtypes(include=['number']).columns
+    means = df_train[num_attr].mean()
+    df_train.fillna(means, inplace=True)
+    df_test.fillna(means, inplace=True)
+
+    categ_attr = df.select_dtypes(exclude=['number']).columns
+    df_train.replace('?', np.nan, inplace=True)
+    df_test.replace('?', np.nan, inplace=True)
+    modes = df_train[categ_attr].mode().iloc[0]
+    df_train.fillna(modes, inplace=True)
+    df_test.fillna(modes, inplace=True)
+
+    # induce rules
+    prism = RandomTree(n_attr=5) # DecisionTree() # 
+    t0 = time.time()
+    prism.fit(df_train, target=target)
+    t1 = time.time()
+
+    print(f'Compute time: {round(t1 - t0, 2)}s')
+
+    # apply rules
+    y_pred = prism.predict(df_test)
+    y_true = df_test[target]
+    print('Accuracy:', round(accuracy_score(y_true, y_pred), 4))
+
+
+if __name__ == '__main__':
+    main()
---
+++ tree.py
@@ -0,0 +1,226 @@
+import warnings
+import numpy as np
+import pandas as pd
+import itertools
+
+# try to load progress bars module to show a simple display
+# with the current progress of the algorithm (OPTIONAL)
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = None
+
+
+class Node:
+    condition = None
+    left = None
+    right = None
+    is_leaf = False
+    label = None
+
+    def next(self, x):
+        return self.left if self.condition(x) else self.right
+
+
+def binary_split(seq):
+    v = len(seq)
+    all_partitions = (itertools.combinations(seq, m) for m in range(1, v))
+    all_partitions = itertools.chain.from_iterable(all_partitions)
+    
+    for m in range(2**(v-1) - 1):
+        yield next(all_partitions)
+
+
+def best_split(data: pd.DataFrame, attr, values, target):
+    attr_col = data[attr]
+    n = len(data)
+    
+    split_scores = []
+    for subset in binary_split(values):
+        mask = attr_col.isin(subset)
+        l = data[mask]
+        r = data[~mask]
+        lscore, llabels = gini_index(l[target])
+        rscore, rlabels = gini_index(r[target])
+        score = (len(l) / n) * lscore + (len(r) / n) * rscore
+        split_scores.append((subset, score, l, r, llabels, rlabels))
+
+    return min(split_scores, key=lambda x: x[1])
+
+
+def gini_index(data: pd.DataFrame):
+    labels, counts = np.unique(data.values, return_counts=True)
+    pr = counts / len(data)
+    return 1 - np.square(pr).sum(), labels
+
+
+class DecisionTree:
+
+    def fit(self, data: pd.DataFrame, target='class'):
+        # precompute attribute types because it seems to be slow
+        attr_cols = data.drop(target, axis=1)
+        self._categ_cols = set(attr_cols.select_dtypes(exclude=['number']))
+        self._num_cols = set(attr_cols.select_dtypes(include=['number']))
+
+        self.root_ = self._build_tree(data.copy(), target)
+        return self
+
+    def predict(self, data: pd.DataFrame):
+        labels = []
+        for _, ins in data.iterrows():
+            node = self.root_
+            while not node.is_leaf:
+                node = node.next(ins)
+            labels.append(node.label)
+
+        return np.array(labels)
+
+    def _build_tree(self, data, target):
+        root = Node()
+        nodes_to_split = [(root, data)]
+
+        while len(nodes_to_split) > 0:
+            node, data = nodes_to_split.pop()
+
+            data, attrs, vals = self._get_splittable_data(data, target)
+
+            if len(attrs) == 0:
+                node.is_leaf = True
+                node.label = data[target].mode()[0]
+                continue
+
+            attrs = self._get_attr_subset(attrs)
+
+            # binarize numerical attributes
+            # num_attrs = data[attrs].select_dtypes(include=['number'])
+            num_cols = list(self._num_cols.intersection(attrs))
+            num_attrs = data[num_cols]
+            
+            if not num_attrs.empty:
+                data_categ, means = self._bin_numerical_attrs(data, num_attrs)
+            else:
+                data_categ = data
+                
+            # find the best splitting attribute
+            attr, val_subset, left_split, right_split, llabels, rlabels = \
+                self._find_best_split(data_categ, attrs, vals, target)
+
+            if attr in num_cols:
+                mean = means[attr]
+                node.condition = lambda x: x[attr] <= mean
+            else:
+                node.condition = lambda x: np.isin(x[attr], val_subset)
+
+            if len(llabels) == 1:
+                node.left = Node()
+                node.left.is_leaf = True
+                node.left.label = llabels[0]
+            else:
+                node.left = Node()
+                nodes_to_split.append((node.left, left_split))
+
+            if len(rlabels) == 1:
+                node.right = Node()
+                node.right.is_leaf = True
+                node.right.label = rlabels[0]
+            else:
+                node.right = Node()
+                nodes_to_split.append((node.right, right_split))
+            
+        return root
+
+    def _get_splittable_data(self, data, target):
+        categ_attrs = self._categ_cols.intersection(data.columns)
+        num_attrs = self._num_cols.intersection(data.columns)
+        
+        # include only categ attributes with multiple values
+        vals = {}
+        for attr in categ_attrs:
+            unique_vals = pd.unique(data[attr])
+            if len(unique_vals) > 1:
+                vals[attr] = tuple(unique_vals)
+
+        # include all numerical attributes
+        for attr in num_attrs:
+            vals[attr] = (True, False)
+
+        useful_attrs = list(vals.keys())
+        useful_cols = useful_attrs + [target]
+        return data[useful_cols], useful_attrs, vals
+
+    def _get_attr_subset(self, attrs):
+        return attrs
+
+    def _find_best_split(self, data, attrs, vals, target):
+        splits = [(attr,) + best_split(data, attr, vals[attr], target) 
+                  for attr in attrs]
+        best_attr, val_subset, score, l, r, llabels, rlabels \
+             = max(splits, key=lambda x: x[2])
+        return best_attr, val_subset, l, r, llabels, rlabels
+
+    def _bin_numerical_attrs(sefl, data, num_attrs):
+        means = num_attrs.mean()
+        data = data.copy()
+        data[num_attrs.columns] = num_attrs <= means
+        return data, means
+
+
+
+class RandomTree(DecisionTree):
+    def __init__(self, n_attr):
+        super().__init__()
+        self.n_attr = n_attr
+
+    def _get_attr_subset(self, attrs):
+        if len(attrs) <= self.n_attr:
+            return attrs
+        else:
+            return np.random.choice(attrs, self.n_attr, replace=False)
+        
+
+
+        # data, attrs, vals = self._get_splittable_data(data, target)
+
+        # if len(attrs) == 0:
+        #     node = Node()
+        #     node.is_leaf = True
+        #     node.label = data[target].mode()[0]
+        #     return node
+
+        # attrs = self._get_attr_subset(attrs)
+
+        # # binarize numerical attributes
+        # num_attrs = data[attrs].select_dtypes(include=['number'])
+        # if not num_attrs.empty:
+        #     data_categ, means = self._bin_numerical_attrs(data, num_attrs)
+        # else:
+        #     data_categ = data
+            
+        # # find the best splitting attribute
+        # attr, val_subset, llabels, rlabels = \
+        #     self._find_best_split(data_categ, attrs, vals, target)
+
+        # node = Node()
+        # if attr in num_attrs.columns:
+        #     mean = means[attr]
+        #     node.condition = lambda x: x[attr] <= mean
+        # else:
+        #     node.condition = lambda x: np.isin(x[attr], val_subset)
+
+        # if len(llabels) == 1:
+        #     node.left = Node()
+        #     node.left.is_leaf = True
+        #     node.left.label = llabels[0]
+        # else:
+        #     left_split = data[node.condition(data)]
+        #     node.left = self._build_tree(left_split, target)
+
+        # if len(rlabels) == 1:
+        #     node.right = Node()
+        #     node.right.is_leaf = True
+        #     node.right.label = rlabels[0]
+        # else:
+        #     right_split = data[~node.condition(data)]
+        #     node.right = self._build_tree(right_split, target)
+            
+        # return node
