f911e935e1a53e02f92a51fe89e04ae5422ae172
Pol Roca <pol.roca@estudiants.urv.cat>
2021-05-04 21:59:16 +0200

    add output file option, add results

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import click
 from click import Choice
+import json
 
 
 def load_hepatitis():
@@ -35,7 +36,8 @@ def load_nursery():
     return df
 
 
-@click.command()
+@click.command(help='Train and test a decision or random forest classifier with the'
+               ' specified parameters on a given dataset (80/20, train/test).')
 @click.option('-d', '--dataset', type=Choice(['hepatitis', 'cmc', 'nursery']), required=True,
               help='Dataset name.')
 @click.option('-c', '--classifier-type', type=Choice(['decision', 'random']), required=True,
@@ -44,11 +46,12 @@ def load_nursery():
               ' (weak classifiers) for the forest (ensemble).')
 @click.option('-f', '--n-features', type=int, default=None, help='Size of the'
               ' subset of features that each tree or node will use. A value of 0 will'
-              ' make this number to be drawn from an uniform distribution for each tree'
+              ' make this number to be drawn from a uniform distribution for each tree'
               ' (only works with decision forests). By default all features will be used.')
 @click.option('-s', '--seed', type=int, default=None, help='Random seed used'
               ' for sampling, shuffling, or any op involving randomness.')
-def main(dataset, classifier_type, n_trees, n_features, seed):
+@click.option('-o', '--out', type=str, default=None, help='File to save output to as JSON.')
+def main(dataset, classifier_type, n_trees, n_features, seed, out):
     # load the corresponding dataset into a dataframe
     if dataset == 'hepatitis':
         df = load_hepatitis()
@@ -66,15 +69,15 @@ def main(dataset, classifier_type, n_trees, n_features, seed):
 
     if n_features is None:
         n_features = len(df.columns) - 1
+    elif n_features < 0:
+        raise ValueError('Option "--n-features" must be higher than 1'
+                         ' (or 0 only if it is a decision forest).')
 
     if classifier_type == 'decision':
         n_features = 'unif' if n_features == 0 else n_features
         clf = DecisionForest(n_trees=n_trees, n_feat=n_features, 
                              random_state=random_state)
     else:
-        if n_features < 1:
-            raise ValueError('Option "--n-features" must be higher than 0'
-                             ' for a random forest classifier.')
         clf = RandomForest(n_trees=n_trees, n_feat=n_features, 
                            random_state=random_state)
         
@@ -99,20 +102,36 @@ def main(dataset, classifier_type, n_trees, n_features, seed):
     # induce forest
     t0 = time.time()
     clf.fit(df_train, target=target)
-    t1 = time.time()
-    print(f'Training time: {round(t1 - t0, 2)}s')
+    train_time = time.time() - t0
+    print(f'Training time: {round(train_time, 2)}s')
     print('\nFeature importance:')
-    print(clf.feature_importance_.sort_values().to_string())
-    print(f'Total: {clf.feature_importance_.sum()}\n')
+    feature_importance = clf.feature_importance_.copy()
+    feature_importance['__total__'] = feature_importance.sum()
+    print(feature_importance.sort_values().to_string(), '\n')
 
     # classify using forest
     t0 = time.time()
     y_pred = clf.predict(df_test)
-    t1 = time.time()
-    print(f'Inference time: {round(t1 - t0, 2)}s')
+    infer_time = time.time() - t0
+    print(f'Inference time: {round(infer_time, 2)}s')
 
     y_true = df_test[target]
-    print('\nAccuracy:', round(accuracy_score(y_true, y_pred), 4))
+    accuracy = accuracy_score(y_true, y_pred)
+    print('\nAccuracy:', round(accuracy, 4))
+
+    if out is not None:
+        result = {
+            'dataset': dataset,
+            'classifier_type': classifier_type,
+            'n_trees': n_trees,
+            'n_features':n_features,
+            'train_time': train_time,
+            'infer_time': infer_time,
+            'accuracy': accuracy,
+            'feature_importance': feature_importance.to_dict()
+        }
+        with open(out, 'a') as f:
+            f.write(json.dumps(result, indent=4) + ',\n')
 
 
 if __name__ == '__main__':
